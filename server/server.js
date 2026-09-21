bd96aef947c496f29f86b66dbd803d3fd4b5416f
Enzo <enzo@gmail.com>
2026-09-21 11:53:26 +0100

    sructure done

--- server/server.js
+++ server/server.js
@@ -2,15 +2,24 @@ import express from "express";
 import "dotenv/config"
 import cors from 'cors'
 import cookieParser from "cookie-parser";
+import { initDB } from "./Config/db.js";
+import { clerkMiddleware } from '@clerk/express'
+import { handleClerkWebhook } from "./Controllers/webhookController.js";
 
 
 const app = express();
+
+//Neon Connection and Table Initialization
+initDB()
 
 const allowedOrigins = process.env.ORIGINS.split(",")
 app.use(cors({origin: "", credentials: true}))
 app.use(cookieParser())
 
+app.use('/api/clerk', express.raw({type: 'application/json'}), handleClerkWebhook)
+
 app.use(express.json())
+app.use(clerkMiddleware())
 
 app.get("/", (req, res) => res.send("API is live"))
 
